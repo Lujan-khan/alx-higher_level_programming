b312c467331ae85b77b46ad0f1195e1cb757b122
Lujan-khan <107659128+Lujan-khan@users.noreply.github.com>
2022-12-23 01:06:38 +0100

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-'use strict';
-let x = process.argv[2];
-function factorial (x) {
-  if (isNaN(x) || x === 1) {
+const a = process.argv[2];
+function factorial (a) {
+  if (isNaN(a) || a === 1) {
     return (1);
   } else {
-    return (x * factorial(x - 1));
+    return (a * factorial(a - 1));
   }
 }
-console.log(factorial(parseInt(x)));
+console.log(factorial(parseInt(a)));
