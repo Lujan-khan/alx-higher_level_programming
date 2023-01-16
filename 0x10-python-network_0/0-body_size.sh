d2a7e6abdacd45168240c93c1f4f253f4864bafd
Lujan-khan <107659128+Lujan-khan@users.noreply.github.com>
2023-01-16 17:52:49 +0100

    new

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the size of the HTTP response
-curl -s "$1" | wc -c
+# Script that takes in a URL, sends a request and displays the size of the body of the response
+curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a DELETE request
-curl -sX DELETE "$1"
+# Script sends a DELETE request to URL passed as the first argument
+curl -sX "DELETE" "$1"
